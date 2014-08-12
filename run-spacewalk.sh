ba2516d943fd1c67bd9adb55a7273bfb63942276
James <jamesnetherton@gmail.com>
2014-08-12 21:24:55 +0100

    Fix postgres process not exiting cleanly

--- run-spacewalk.sh
+++ run-spacewalk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sed -i 's/shared_buffers = 384MB/shared_buffers = 100MB/g' /var/lib/pgsql/data/postgresql.conf
-sysctl -w kernel.shmmax=134217728
+sysctl -w kernel.shmmax=134217728 > /dev/null
 
 exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
 
